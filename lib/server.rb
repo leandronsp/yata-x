57e2baf3ddb755ead1446805a9673d66ab7e4637
leandronsp <leandronsp@gmail.com>
2022-04-05 21:23:11 -0300

    wip: initial implementation for login

--- lib/server.rb
+++ lib/server.rb
@@ -1,4 +1,5 @@
 require 'socket'
+require 'cgi'
 
 PORT = 3000
 socket = TCPServer.new('0.0.0.0', PORT)
@@ -11,24 +12,55 @@ loop do
 
   # Request
   request = ''
+  headers = {}
+  body    = ''
+  params  = {}
+  cookies = {}
 
   while line = client.gets
     break if line == "\r\n"
     request += line
+
+    # Request headers
+    header_key, header_value = line.split(': ')
+    headers[header_key] = header_value
   end
 
   puts request
   puts "\n"
 
-  # Response
-  response = """
-  HTTP/2.0 200\r\n
-  \r\n
-  \r\n
-  <h1>Hello</h1>
-  """
+  # Request body
+  content_length = headers['Content-Length']
+  body = client.read(content_length.to_i) if content_length
 
-  client.puts(response)
+  # Extract params from body
+  body_parts = body.split('&')
+  params = body_parts
+    .map { |part| part.split('=').map(&CGI.method(:unescape)) }
+    .to_h
+
+  # Request cookies
+  if cookie = headers['Cookie']
+    cookie_name, cookie_value = cookie.split('=')
+    cookies[cookie_name] = CGI.unescape(cookie_value)
+  end
+
+  cookies['email'] ||= params['email']
+
+  # Response
+  response = %{
+HTTP/2.0 200\r\n
+Content-Type: text/html\r\n
+Set-Cookie: email=#{cookies['email']}; HttpOnly\r\n
+\r\n
+<form action="/" method="POST">
+  <input type="text" placeholder="Email" name="email"/>
+  <input type="password" placeholder="Password" name="password"/>
+  <input type="submit" value="Login"/>
+</form>
+}
+
+  client.puts(response.strip.gsub(/\n+/, "\n"))
 
   # Close connection
   client.close
